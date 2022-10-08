70a47e0d37bbeb0e08e0d09678aaddfc49f7b0fe
sietbhavya <bhavyahp02@gmail.com>
2022-10-08 19:27:27 +0530

    First Commit

---
+++ Springioc/src/main/java/com/tns/springioc/Airtel.java
@@ -0,0 +1,13 @@
+package com.tns.springioc;
+
+public class Airtel implements Sim {
+
+	public void calling() {
+		System.out.println("Calling using Airtel sim");
+		
+	}
+
+	public void data() {
+		System.out.println("Browsing  using Airtel sim");
+	}
+}
---
+++ Springioc/src/main/java/com/tns/springioc/Mobile.java
@@ -0,0 +1,18 @@
+package com.tns.springioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		ApplicationContext c=new ClassPathXmlApplicationContext("beans.xml");
+		System.out.println("Config loaded");
+		
+		Sim s= c.getBean("sim",Sim.class);
+		s.calling();
+		s.data();
+		
+		
+	}
+}
